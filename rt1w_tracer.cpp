b361aafd399ba594864cf46da10235c693d4bbb8
Esmé Puzio <esmepuzio@Esmes-MacBook-Pro.local>
2023-11-02 18:14:46 +0000

    Implemented step 4 of RT1W, pretty gradient?

--- rt1w_tracer.cpp
+++ rt1w_tracer.cpp
@@ -1,15 +1,40 @@
 #include "color.h"
 #include "vec3.h"
+#include "ray.h"
 
 #include <iostream>
 using namespace std;
 
+color ray_color(const ray& r){
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+}
+
 int main() {
 
     // Image
+    auto aspect_ratio = 16.0 / 9.0;
+    int image_width = 400;
+    int image_height = static_cast<int>(image_width / aspect_ratio);
+    if (image_height < 1){image_height = 1;}
 
-    int image_width = 256;
-    int image_height = 256;
+    // Camera
+
+    auto focal_length = 1.0;
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * (static_cast<double>(image_width)/image_height);
+    auto camera_center = point3(0, 0, 0);
+
+    // Camera vectors from edge to edge
+    auto viewport_u = vec3(viewport_width, 0, 0);
+    auto viewport_v = vec3(0, -viewport_height, 0);
+
+    auto pixel_delta_u = viewport_u / image_width;
+    auto pixel_delta_v = viewport_v / image_height;
+
+    auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
     // Render
 
@@ -18,7 +43,7 @@ int main() {
     for (int j = 0; j < image_height; ++j) {
         clog << "[" << ((j*1.0) / image_height) << "%]" << "\r REMAINING LINES: " << (image_height - j) << ' ' << flush;
         for (int i = 0; i < image_width; ++i) {
-            auto pixel_color = color(double(i)/(image_width-1), double(j)/(image_height-1), double(i)/(256-image_width));
+            auto pixel_color = color(double(i)/(image_width-1), double(j)/(image_height-1), 0);
             write_color(std::cout, pixel_color);
         }
     }
