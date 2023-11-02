db192e937a09309e6b461040865f08d9aa827713
Esmé Puzio <esmepuzio@Esmes-MacBook-Pro.local>
2023-11-02 17:08:22 +0000

    example 1 from rt1w added

---
+++ ppm_example_rt1w.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Image
+
+    int image_width = 256;
+    int image_height = 256;
+
+    // Render
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = 0; j < image_height; ++j) {
+        for (int i = 0; i < image_width; ++i) {
+            double r = double(i) / (image_width-1); //had to set as double
+            double g = double(j) / (image_height-1);
+            double b = 0;
+
+            int ir = static_cast<int>(255.999 * r);
+            int ig = static_cast<int>(255.999 * g);
+            int ib = static_cast<int>(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << std::flush;
+        }
+    }
+}
