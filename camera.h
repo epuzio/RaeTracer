df4b4b6857499f11cec56ddbd99387af5a3bc728
Esmé Puzio <esmepuzio@Esmes-MacBook-Pro.local>
2023-11-14 21:14:34 +0000

    pushing LLM rewrites, going home for the evening

--- camera.h
+++ camera.h
@@ -5,74 +5,63 @@
 
 #include "color.h"
 #include "shapes_hit.h"
+#include "vec3.h"
 
 #include <iostream>
+#include <vector>
+#include <cmath>
+
 using namespace std;
 
 class camera {
+    private: 
+        //set from JSON file in main:
+        int width;
+        int height;
+        point3 position; //camera position as a point in x,y,z space
+        vec3 lookAt; //vector pointing to spot camera is looking at
+        vec3 upVector; //normalized y-axis vector [0, 1, 0]
+        vec3 right, up, forward; //basis vectors for camera position
+        double fov;
+        double exposure;   
+
   public:
+  Camera(int w, int h, const vec3& pos, const vec3& look, const vec3& upVec, double fieldOfView, double exp)
+        : width(w), height(h), position(pos), lookAt(look), upVector(upVec), fov(fieldOfView), exposure(exp) {
+        
+        // Calculate basis vectors for camera orientation
+        forward = (lookAt - position).normalized();
+        right = forward.cross(upVector).normalized();
+        up = right.cross(forward).normalized();
+    }
+
     // Camera dimensions
-    auto aspect_ratio = 16.0 / 9.0;
+    double aspect_ratio = 16.0 / 9.0;
     int image_width = 400;
+    int samplesPerPixel;
    
-    void render(const hittable& world) {
-        initialize();
-        cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+   vec3 generateRays() const {
+        vec3 rays;
 
-        //Writing to output + progress bar
-        for (int j = 0; j < image_height; ++j) {
-            clog << "[" << ((j*1.0) / image_height) << "%]" << "\r REMAINING LINES: " << (image_height - j) << ' ' << flush;
-            for (int i = 0; i < image_width; ++i) {
-                auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-                auto ray_direction = pixel_center - camera_center;
-                ray r(camera_center, ray_direction);
+        double aspectRatio = static_cast<double>(width) / height;
+        double halfHeight = tan(fov * 0.5 * M_PI / 180.0);
+        double halfWidth = aspectRatio * halfHeight;
 
-                color pixel_color = ray_color(r, world); 
-                write_color(std::cout, pixel_color);
+        vec3 topLeft = position + forward - halfWidth * right + halfHeight * up;
+        double pixelWidth = 2.0 * halfWidth / width;
+        double pixelHeight = 2.0 * halfHeight / height;
+
+        for (int y = 0; y < height; ++y) {
+            for (int x = 0; x < width; ++x) {
+                double u = (x + 0.5) * pixelWidth - halfWidth;
+                double v = (y + 0.5) * pixelHeight - halfHeight;
+
+                vec3 rayDirection = (topLeft + u * right - v * up) - position;
+                rays.push_back(normalize(rayDirection));
             }
         }
-        clog << "\r[100%] DONE :~)                    \n" << flush;
-    }
 
-  private:
-    int image_height;   // Rendered image height
-    point3 center;         // Camera center
-    point3 pixel00_loc;    // Location of pixel 0, 0
-    vec3 pixel_delta_u;  // Offset to pixel to the right
-    vec3 pixel_delta_v;  // Offset to pixel below
-
-    void initialize() {
-        // Set height based on width and aspect ratio
-        int image_height = static_cast<int>(image_width / aspect_ratio);
-        if (image_height < 1){image_height = 1;} //make sure heignt is at least 1
-
-        center = point3(0, 0, 0);
-
-        auto focal_length = 1.0;
-        auto viewport_height = 2.0;
-        auto viewport_width = viewport_height * (static_cast<double>(image_width)/image_height);
-
-        // Camera vectors from edge to edge
-        auto viewport_u = vec3(viewport_width, 0, 0);
-        auto viewport_v = vec3(0, -viewport_height, 0);
-
-        auto pixel_delta_u = viewport_u / image_width;
-        auto pixel_delta_v = viewport_v / image_height;
-
-        auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
-        auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
-
-    }
-
-    color ray_color(const ray& r, const hittable& world) const {
-        hit_record rec;
-        if (world.hit(r, interval(0, infinity), rec){
-            return 0.5*(rec.normal+color(1,1,1));
-        }) 
-
-        vec3 unit_direction = unit_vector(r.direction());
-        auto a = 0.5*(unit_direction.y() + 1.0);
-        return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+        return rays;
     }
 };
 
--- raytracer.cpp
+++ raytracer.cpp
@@ -1,26 +1,59 @@
-#include "raytracer.h"
-#include "camera.h"
-#include "color.h"
-#include "shape.h"
-#include "shapes_hit.h"
-#include "sphere.h"
+// #include "raytracer.h"
+// #include "camera.h"
+// #include "color.h"
+// #include "shape.h"
+// #include "shapes_hit.h"
+// #include "sphere.h"
 
-#include "interval.h"
-#include "vec3.h"
-#include "ray.h"
+// #include "interval.h"
+// #include "vec3.h"
+// #include "ray.h"
+
+#include <nlohmann/json.hpp>
+
 
 #include <iostream>
 using namespace std;
+using json = nlohmann::json;
 
 int main() {
-    shapes_hit world;
+    Camera camera;
 
-    world.add(make_shared<sphere>(point3(0, 0, -1), .5));
-    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+    //JSON input
+    ifstream inputFile("binary_sphere.json");
+    json input;
 
-    camera camera;
-    camera.aspect_ratio = 16.0/9.0;
-    camera.image_width = 400;
+    if (inputFile.is_open()) {
+        try {
+            inputFile >> input; // Parse JSON from file
+            json cameraInput = input["camera"]
+            cout << cameraInput << endl;
 
-    camera.render(world);
+            //Set camera variables from JSON input:
+            // camera = new Camera(cameraInput);
+            // camera = new Camera("pinhole", cameraInput["width"], cameraInput["height"], 
+            // cameraInput["position"], cameraInput["lookAt"], cameraInput["upVector"],
+            // cameraInput["fov"], cameraInput["exposure"]);
+
+        } catch (json::parse_error& e) {
+            cerr << "Parse error: " << e.what() << endl;
+        }
+    } else {
+        cerr << "Unable to open json file." << endl;
+    }
+
+
+    // shapes_hit world;
+
+    // world.add(make_shared<sphere>(point3(0, 0, -1), .5));
+    // world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+
+
+    // camera.aspect_ratio = 16.0/9.0;
+    // camera.image_width = 400;
+
+    // camera.render(world);
+
+    
 }
+    
